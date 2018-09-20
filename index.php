224b65ced39e0923ca19a780883e7147726b37d1
大川裕也 <bluepencil.writes@gmail.com>
2018-09-20 16:17:13 +0900

    フロント部分のマージしました確認お願いします。

--- index.php
+++ index.php
@@ -3,16 +3,53 @@
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="content-type">
     <meta content="width=device-width,user-scalable=no,maximum-scale=1" name="viewport">
+    <!-- フロント側CSS -->
+    <link rel="stylesheet" href="fukidasi.css">
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
 
-    <title>チャットあｄ</title>
+    <title>ここにサービスタイトル</title>
  </head>
- 会話<br>
+<body>
+    <section class="page">
+        <section class="icon">  
+            <div class="kaiwa">
+                <figure class="kaiwa-img-left">
+                    <img src="img/user.PNG" alt="no-img2″>
+                    <figcaption class="kaiwa-img-description">
+                        
+                    </figcaption>
+                </figure>
+                <div class="kaiwa-text-right">
+                    <p class="kaiwa-text">
+                        おはよー
+                    </p>
+                </div>
+            </div>
+        </section>
 
-<form method="post" action="index.php">
-名前 <input type="text" name="naame">
-	テキスト <input type="text" name="chaat">
-	<button name="sending" type="submit">送信</button>
-</form>	
+
+
+
+        <section class="talk" id="footer">
+            <form class="pure-form" method="post" action="index.php">   
+                <fieldset>
+                    <div class="pure-u-20-24">
+                        <input name="chaat" style="width: 100%;" type="text" placeholder="悩みを話す">
+                    </div>
+                    <div class="pure-u-2-24">
+                        <button name="sending" type="submit" class="pure-button pure-button-primary " style="margin: 0px;" onclick="talkPost()">話す</button>
+                    </div>
+                </fieldset>
+            </form>
+        </section>
+
+
+
+
+
+</section>
+
+
 <?php
 	 $db_ac = 'mysql:dbname=dcm1;host=localhost';
      $user_name = 'root';// onload="mapp1()"
@@ -62,3 +99,4 @@
 ?>
 
 </body>
+</html>
