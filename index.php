19192fde775e62561b532523ec1fdc6e5cc6f6b4
NaokiOtammni <200296s@ugs.kochi-tech.ac.jp>
2018-09-20 22:16:13 +0900

    完成したと思いたい

--- index.php
+++ index.php
@@ -6,17 +6,70 @@
     <!-- フロント側CSS -->
     <link rel="stylesheet" href="fukidasi.css">
     <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
-
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+			  crossorigin="anonymous"></script>
+		
     <title>ここにサービスタイトル</title>
  </head>
 <body>
+<?php
+	 $db_ac = 'mysql:dbname=dcm1;host=localhost';
+     $user_name = 'root';// onload="mapp1()"
+     $pass = 'Zx2lh46j2tts';
+    $url='../../ahaha.json';
+ 
+ 	try{
+                    $db_key = new PDO($db_ac, $user_name, $pass);
+                    
+                    if ($db_key == null) {
+                        print('接続に失敗しました。<br>');
+                    }
+                     $db_key->query('SET NAMES utf8');
+                     
+                    if((!empty($_POST['chaat']))){
+                    	$quer= "SELECT id from comment_data";
+                    	$flag = $db_key->query($quer);
+                        //print $flg_json;
+
+                    	foreach( $flag as $value ) 
+                    	{		
+                    		$lds=$value[id];
+                    	}      
+                    	$lds=$lds+1;
+                   	$Ids=(int)$Ids;
+			$quer = "insert into comment_data (id, name, comment, time, user_id) values (?,?,?,?,?)";
+			$stmt = $db_key->prepare($quer);
+                         $flag1 = $stmt->execute(array($lds, $_POST['chaat'], 'aa',  date('Y/m/d'), 1));
+                    	if($flag1){
+                    		//echo '投稿完了しました';
+                    	}else{
+                    		echo '投稿NG';
+                    	}
+                    	$quer= "SELECT name, comment, time from comment_data ";
+                    	$flag = $db_key->query($quer);
+                    	$jsonn=$flag->fetchAll(PDO::FETCH_ASSOC);
+                    	$flg_json = json_encode($jsonn,JSON_UNESCAPED_UNICODE); 
+                    } else {
+                    	$quer= "SELECT name, comment, time from comment_data ";
+                    	$flag = $db_key->query($quer);
+                    	 $i=0;
+                    	$jsonn=$flag->fetchAll(PDO::FETCH_ASSOC);
+                    	$flg_json = json_encode($jsonn,JSON_UNESCAPED_UNICODE); 
+                    	//echo $flg_json;
+                   }
+}catch (PDOException $e){
+                                print('Error:'.$e->getMessage());
+                                die();
+                        }
+
+?>
     <section class="page">
         <section class="icon">  
             <div class="kaiwa">
                 <figure class="kaiwa-img-left">
-                    <img src="img/user.PNG" alt="no-img2″>
+                    <img src="img/user.PNG" alt="no-img2">
                     <figcaption class="kaiwa-img-description">
-                        
                     </figcaption>
                 </figure>
                 <div class="kaiwa-text-right">
@@ -28,105 +81,21 @@
         </section>
 
     <section class="talkView">
-
-
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////////////
-                </p>
-            </div>
+        <div id="kaiwa">
         </div>
-        
+        <script type="text/javascript">
+var js_array = JSON.parse('<?php echo $flg_json; ?>');
+console.log(js_array);
+for (var i=0; i<js_array.length; i++){
+         $("#kaiwa").append(' <div class="kaiwa-text-left"> <div class="kaiwa-text">ユーザ名:'+js_array[i].name+'<br>コメント:'+js_array[i].comment+'<br>日付:'+js_array[i].time+'</div></div><br>');
+}
+</script>
+
         <div class="kaiwa-text-right">
             <p class="kaiwa-text">
                 おはよー
             </p>
         </div>
-        
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////////////
-                </p>
-            </div>
-        </div>
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////////////
-                </p>
-            </div>
-        </div>
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////////////
-                </p>
-            </div>
-        </div>
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////////////
-                </p>
-            </div>
-        </div>
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////////////
-                </p>
-            </div>
-        </div>
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////////////
-                </p>
-            </div>
-        </div>
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////////////
-                </p>
-            </div>
-        </div>
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////////////
-                </p>
-            </div>
-        </div>
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////2////////
-                </p>
-            </div>
-        </div>
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////////////
-                </p>
-            </div>
-        </div>
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////////////
-                </p>
-            </div>
-        </div>
-        <div class="kaiwa"">
-            <div class="kaiwa-text-left">
-                <p class="kaiwa-text">
-                    /////////チャットの内容////2////////
-                </p>
-            </div>
         </div>
     </section>
 
@@ -142,62 +111,11 @@
                     </div>
                 </fieldset>
             </form>
-        </section>
-
-
-
-
+        </section>     
 
 </section>
 
 
-<?php
-	 $db_ac = 'mysql:dbname=dcm1;host=localhost';
-     $user_name = 'root';// onload="mapp1()"
-     $pass = 'Zx2lh46j2tts';
-    $url='../../ahaha.json';
- 
- 	try{
-                    $db_key = new PDO($db_ac, $user_name, $pass);
-                    
-                    if ($db_key == null) {
-                        print('接続に失敗しました。<br>');
-                    }
-                     $db_key->query('SET NAMES utf8');
-                     
-                    if((!empty($_POST['chaat'])) && (!empty($_POST['naame']))){
-                    	$quer= "SELECT id from comment_data";
-                    	$flag = $db_key->query($quer);
-                    	foreach( $flag as $value ) 
-                    	{		
-                    		$lds=$value[id];
-                    	}      
-                    	$lds=$lds+1;
-                   	$Ids=(int)$Ids;
-			$quer = "insert into comment_data (id, name, comment, time, user_id) values (?,?,?,?,?)";
-			$stmt = $db_key->prepare($quer);
-                                $flag1 = $stmt->execute(array($lds, $_POST['chaat'], $_POST['naame'],  date('Y/m/d'), 1));
-        
-                    	if($flag1){
-                    		//echo '投稿完了しました';
-                    	}else{
-                    		echo '投稿NG';
-                    	}
-                    } else {
-                    	$quer= "SELECT * from comment_data ";
-                    	$flag = $db_key->query($quer);
-                    	foreach( $flag as $value ) 
-                    	{		
-                    		print "$value[name],$value[comment],$value[time]<br>";
-                   	}      
-
-                   }
-}catch (PDOException $e){
-                                print('Error:'.$e->getMessage());
-                                die();
-                        }
-
-?>
 
 </body>
 </html>
